f08d8a42e36c68b5ed8a1e6334e7996d73a7435b
diponkorroy064-max <diponkorroy064@gmail.com>
2026-02-22 21:00:40 +0600

    add toggling

--- file.js
+++ file.js
@@ -26,5 +26,47 @@ document.getElementById('btn-all').addEventListener('click',   function() {
 })
 
 
+const totalCount = document.getElementById('totalCount');
+const interviewCount = document.getElementById('interviewCount');
+const rejectedCount = document.getElementById('rejectedCount');
+
+let interviewList = [];
+let rejectedList = [];
+
+function calculateCount() {
+    totalCount.innerText = cardSection.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateCount();
+
+const mainContainer = document.querySelector('main');
+// console.log(mainContainer);
+
+
+const allBtn = document.getElementById('btn-all');
+const interviewBtn = document.getElementById('interview-btn');
+const rejectedBtn = document.getElementById('rejected-btn');
+
+
+function toggleStyle(id) {
+    allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+    interviewBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+    rejectedBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+    allBtn.classList.add('text-[#64748B]', 'bg-white');
+    interviewBtn.classList.add('text-[#64748B]', 'bg-white');
+    rejectedBtn.classList.add('text-[#64748B]', 'bg-white');
+
+    let selected = document.getElementById(id);
+    // console.log(selected);
+
+    selected.classList.add("bg-[#3B82F6]", "text-white");
+    selected.classList.remove("text-[#64748B]", "bg-white");
+
+}
+
 
 
